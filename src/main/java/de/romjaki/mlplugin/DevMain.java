f4fad4097844d05e706094fcd5193c53eb750d22
Tyrannova <daniel.sluga@grb-online.net>
2019-01-13 11:01:28 +0100

    Initial commit

---
+++ src/main/java/de/romjaki/mlplugin/DevMain.java
@@ -0,0 +1,12 @@
+package de.romjaki.mlplugin;
+
+import de.romjaki.pluggabledino.MainKt;
+
+public class DevMain {
+
+    public static void main(String[] args) {
+        MainKt.main(new String[]{"dev", MLPluginBase.class.getCanonicalName()});
+    }
+
+
+}
---
+++ src/main/java/de/romjaki/mlplugin/MLPluginBase.java
@@ -0,0 +1,43 @@
+package de.romjaki.mlplugin;
+
+import de.romjaki.pluggabledino.MainKt;
+import de.romjaki.pluggabledino.api.EventHandler;
+import de.romjaki.pluggabledino.api.IPlugin;
+import de.romjaki.pluggabledino.events.GameLostEvent;
+import de.romjaki.pluggabledino.events.GameRenderEvent;
+import de.romjaki.pluggabledino.events.GameUpdateEvent;
+import org.newdawn.slick.Color;
+
+public class MLPluginBase extends IPlugin {
+
+    private float cactiPosition;
+
+    @EventHandler
+    public void onGameLost(GameLostEvent event) {
+        System.out.println("dein ergnis war " + event.getScore());
+    }
+
+    @EventHandler
+    public void onUpdate(GameUpdateEvent event) {
+        cactiPosition = event.getWorld()
+                .getCacti()
+                .stream()
+                .map(body ->
+                        body.getPosition().x
+                )
+                .min(Float::compareTo)
+                .orElse(10000f);
+        if(cactiPosition < 10f){
+            event.getWorld().tryJump();
+        }
+
+    }
+
+    @EventHandler
+    public void onRender(GameRenderEvent event) {
+        event.getGraphics().setColor(Color.red);
+        event.getGraphics().drawLine(cactiPosition * MainKt.WIDTH / 50, 0, cactiPosition * MainKt.WIDTH / 50, MainKt.HEIGHT);
+    }
+
+
+}
